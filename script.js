d6822d01a7051f4fce2f01ce84dd0461e3b9a81b
AvivGelfand <63909805+AvivGelfand@users.noreply.github.com>
2020-06-19 21:17:11 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -105,6 +105,7 @@ const createCards = (url) => {
                                  
                                 // debugger
                                    
+                                }
                             }else{
                                     alert('new coin!')
                                     // debugger
@@ -144,7 +145,7 @@ const createCards = (url) => {
                                         `);
                                     });
                                 }
-                            }
+
                             });
                     $(`#collapse${j}`).on("hidden.bs.collapse", function(){
                         $(`#loadedCoinData${j}`).remove();
