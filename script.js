c63a7ca7470243c2d4bc0ef083e28cfb6f5c4eb7
AvivGelfand <63909805+AvivGelfand@users.noreply.github.com>
2020-06-20 10:07:51 +0300

    fixed more info options

--- script.js
+++ script.js
@@ -14,12 +14,13 @@ const createCards = (url) => {
                         <div class="card" id=c${j}>
                             <div class="card-body">
                                 <!-- toggle switch -->
-                                
+
                                 <div 
                                 class="custom-control custom-switch  incard" >
                                     <input type="checkbox" class="custom-control-input" id="switch${j}">
                                     <label class="custom-control-label" for="switch${j}"></label>
                                 </div>
+
                                 <h5 class="card-title">${coinsData[j].symbol}</h5>
                                 <p class="card-text">${coinsData[j].name}</p>
                                 <button class="btn btn-primary" id="btn${j}" data-toggle="collapse" href="#collapse${j}" role="button" aria-expanded="false" aria-controls="collapse${j}">More Info</button>
@@ -104,8 +105,6 @@ const createCards = (url) => {
                                     }   
                                  
                                 // debugger
-                                   
-                                }
                             }else{
                                     alert('new coin!')
                                     // debugger
@@ -145,6 +144,45 @@ const createCards = (url) => {
                                         `);
                                     });
                                 }
+                            }else{
+                                alert('new coin!')
+                                // debugger
+                                let url2 = `https://api.coingecko.com/api/v3/coins/${coinsData[j].id } `
+                                $.get(url2).then(function(coin){
+                                    // debugger
+                                    let d = new Date();
+                                    let n = d.getTime();
+                                    // console.log(coin);
+                                    let coinDataObj = {
+                                        name: coin.name.toLowerCase(),
+                                        id: coin.id.toLowerCase(),
+                                        usdPrice: coin.market_data.current_price.usd,
+                                        euroPrice: coin.market_data.current_price.eur,
+                                        ilsPrice: coin.market_data.current_price.ils,
+                                        image: coin.image.small,
+                                        time: n,
+                                    };
+                                        // 3. arr.push(objet)
+                                        storedCoinsArr.push(coinDataObj);
+                                        // 4. JSON.stringify(arr)
+                                        let strCoinsList = JSON.stringify(storedCoinsArr); 
+                                        // 5.setitem  window.localStorage.setItem('c');
+                                            window.localStorage.setItem('storedCoinsData', strCoinsList);
+                                        // end of local storage part
+                                        // debugger
+
+                                        $(`#loader${j}`).remove();
+                                        $(`#collapse${j}`).append(`
+                                        <div id="loadedCoinData${j}" >
+                                            <br>
+                                            <img src="${coinDataObj.image}" class="img-thumbnail" style="width:3rem; height:3rem;"> 
+                                            <p>USD: ${coinDataObj.usdPrice}$</p>
+                                            <p>EURO: ${coinDataObj.euroPrice}€</p>
+                                            <p>NIS: ${coinDataObj.ilsPrice}₪</p>
+                                        </div>
+                                    `);
+                                });
+                            }
 
                             });
                     $(`#collapse${j}`).on("hidden.bs.collapse", function(){
